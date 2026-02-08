ca1b76d5d62f93a524350f2f30b34f8baf71c45e
Jasaswi das <jasaswid83@gmail.com>
2026-02-08 09:01:10 +0530

    Delete cosmic watch web directory

--- cosmic watch web/backend/src/routes/notification.routes.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import * as notificationController from '../controllers/notification.controller';
-
-const router = express.Router();
-
-router.get('/', notificationController.getNotifications);
-router.put('/:id/read', notificationController.markAsRead);
-
-export default router;
